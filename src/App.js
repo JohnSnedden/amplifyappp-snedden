f0b6064492604821d37f8316f5c5ba341caa7119
john snedden <john.snedden@libertymutual.com>
2019-05-17 13:19:21 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DNA Cloud Concepts
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ggprod"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mark Pevec
         </a>
       </header>
     </div>
